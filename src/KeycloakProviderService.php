e92e939f64cc2cceac3cd8555d5758989fcd723a
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2026-07-24 15:08:36 +0700

    feat: add pagination params first=0&max=-1 to admin API list methods

--- src/KeycloakProviderService.php
+++ src/KeycloakProviderService.php
@@ -373,7 +373,7 @@ class KeycloakProviderService extends AbstractProvider implements ProviderInterf
      */
     public function getClientList(): array
     {
-        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/clients");
+        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/clients?first=0&max=-1");
     }
 
 
@@ -403,7 +403,7 @@ class KeycloakProviderService extends AbstractProvider implements ProviderInterf
      */
     public function getUserList(): array
     {
-        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/users");
+        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/users?first=0&max=-1");
     }
 
     /**
@@ -481,7 +481,7 @@ class KeycloakProviderService extends AbstractProvider implements ProviderInterf
      */
     public function getUserRoles($userUuid): array
     {
-        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/users/{$userUuid}/role-mappings");
+        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/users/{$userUuid}/role-mappings?first=0&max=-1");
     }
 
     /**
@@ -493,7 +493,7 @@ class KeycloakProviderService extends AbstractProvider implements ProviderInterf
 
     public function getClientRoles(): array
     {
-        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/clients/{$this->clientUuid}/roles");
+        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/clients/{$this->clientUuid}/roles?first=0&max=-1");
     }
 
     /**
@@ -505,7 +505,7 @@ class KeycloakProviderService extends AbstractProvider implements ProviderInterf
      */
     public function getUsersWithRole($roleName): array
     {
-        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/roles/{$roleName}/users");
+        return $this->request('GET', "{$this->apiUrl}admin/realms/{$this->realm}/roles/{$roleName}/users?first=0&max=-1");
     }
 
     /**
@@ -1402,6 +1402,10 @@ class KeycloakProviderService extends AbstractProvider implements ProviderInterf
                     'headers' => [
                         'Accept' => 'application/json',
                     ],
+                    'query' => [
+                        'first' => 0,
+                        'max' => -1,
+                    ],
                 ]
             );
 
@@ -1459,6 +1463,10 @@ class KeycloakProviderService extends AbstractProvider implements ProviderInterf
                 [
                     'headers' => [
                         'Accept' => 'application/json',
+                    ],
+                    'query' => [
+                        'first' => 0,
+                        'max' => -1,
                     ],
                 ]
             );
